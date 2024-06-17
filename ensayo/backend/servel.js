c62b985e7fad31d82789310caa9e5afbb11f1987
davidlealo <davidlealo@gmail.com>
2024-06-16 22:11:09 -0400

    error en nombre del archivo

--- ensayo/backend/servel.js
+++ ensayo/backend/servel.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, console.log('Servidor encendido'))
+app.listen(3000, () =>{
+    console.log('Servidor encendido')
+})
 
 app.get("/home", (req, res) => {
     res.send("Hello World Express Js")
