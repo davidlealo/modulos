e6666ed9e89b19582286243afcc1ec5c893d20f7
davidlealo <davidlealo@gmail.com>
2024-06-16 22:07:03 -0400

    revisando error

--- ensayo/backend/server.js
+++ ensayo/backend/server.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, console.log('Hello wordl!'))
+app.listen(3000, console.log('Servidor encendido'))
+
+app.get("/home", (req, res) => {
+    res.send("Hello World Express Js")
+    })
